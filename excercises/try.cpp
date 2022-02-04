ac339044d85ff490b1eb516e3d6bbf05e718132a
Aashish123-aikki <ashishkuchhal68@gmail.com>
2022-02-04 10:25:30 +0530

    3rd commit

--- excercises/try.cpp
+++ excercises/try.cpp
@@ -1,8 +1,40 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
+
 using namespace std;
-
-
+ vector<int> r;
+int count=0;
+bool contain(vector<int> a,int n){
+    for(int i=0;i<a.size();i++){
+        if(a[i]==n){
+            return 1;
+        }
+    }
+    return 0;
+}
+int beautifulFunction(int N){
+   
+    if(contain(r,N)==1){
+        return ;
+    }
+    else{
+        r.push_back(N);
+        N++;
+        string ab=tostring(N);
+        ab.erase(remove(ab.begin(),ab.end(),"0"));
+        stringstream ss(ab);
+        ss>>ab;
+        count++;
+        beautifulFunction(N);
+    }
+    
+    return count;
+ 
+}
 int main(){
-                cout<<(10>>2);
+    int N;
+    cin>>N;
+    int result;
+    result = beautifulFunction(N);
+    cout<<result;
+    return 0;
 }
